9979678bf180a1b01bfa7ba19f1d397e547cf06d
Ankur <ankurshrivastava1987@gmail.com>
2024-12-05 10:38:29 +0530

    Added more test cases

--- Iron.Tests/OldMobileKeypadTests.cs
+++ Iron.Tests/OldMobileKeypadTests.cs
@@ -11,13 +11,13 @@ namespace Iron.Tests
         public void OldKeypadSampleText1()
         {
             //Arrange
-            string sampleText1 = "227*#";
+            string sampleText = "33#";
 
             //Expectedoutput
-            string expectedOutput = "B";
+            string expectedOutput = "E";
 
             //Act
-            string actualOutput = oldMobileKeyPad.ReplaceOldKeypadInputToChar(sampleText1);
+            string actualOutput = oldMobileKeyPad.ReplaceOldKeypadInputToChar(sampleText);
 
             //Assert
             Assert.Equal(expectedOutput, actualOutput);
@@ -25,6 +25,23 @@ namespace Iron.Tests
 
         [Fact]
         public void OldKeypadSampleText2()
+        {
+            //Arrange
+            string sampleText = "227*#";
+
+            //Expectedoutput
+            string expectedOutput = "B";
+
+            //Act
+            string actualOutput = oldMobileKeyPad.ReplaceOldKeypadInputToChar(sampleText);
+
+            //Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+       
+        [Fact]
+        public void OldKeypadSampleText3()
         {
             //Arrange
             string sampleText2 = "4433555 555666#";
@@ -38,5 +55,22 @@ namespace Iron.Tests
             //Assert
             Assert.Equal(expectedOutput, actualOutput);
         }
+
+        [Fact]
+        public void OldKeypadSampleText4()
+        {
+            //Arrange
+            string sampleText = "8 88777444666*664#";
+
+            //Expectedoutput
+            string expectedOutput = "TURINNG";
+
+            //Act
+            string actualOutput = oldMobileKeyPad.ReplaceOldKeypadInputToChar(sampleText);
+
+            //Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
     }
 }
